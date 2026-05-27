bc29054d2b27729e23ec5b6738aa4d028c7f2f8b
HayssousOthmane <jassous.othmane2006@gmail.com>
2026-05-27 20:52:25 +0100

    haha

---
+++ script.js
@@ -0,0 +1,44 @@
+
+// alert("Welcome to the site!");
+
+const name = document.getElementById("nom");
+const password = document.getElementById("password");
+const email = document.getElementById("email");
+const btnSend = document.getElementById("btnSend");
+
+
+btnSend.addEventListener("click", function() {
+
+    if (name.value === "" || password.value === "" || email.value === "") {
+        alert("Please fill in all fields.");
+    
+       
+    }
+     else if (!email.value.includes("@")) {
+            alert("Please enter a valid email address.");
+        }
+         else {
+        alert(name.value + " " + password.value + " " + email.value);
+    }
+});
+
+
+const formInscription = document.getElementById("formSignup");
+
+const signupbutton = document.getElementById("signupbtn");
+
+signupbutton.addEventListener("click", function() {
+    formInscription.style.display = "flex";
+        formConnexion.style.display = "none";
+
+});
+
+const formConnexion = document.getElementById("formSignIn");
+
+const loginbutton = document.getElementById("signinbtn");
+
+loginbutton.addEventListener("click", function() {
+    formConnexion.style.display = "flex";
+        formInscription.style.display = "none";
+
+});
